c7d8027250fc74706fd7c92169f88b8afe1e37c5
Ritik-Raghav <ritikrghv313@gmail.com>
2025-10-07 23:47:05 +0530

    Fix CORS allowed origins by removing trailing slashes

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ app.use(helmet()); // adds secure headers
 
 const allowedOrigins = [
   "http://localhost:8080",
-  "http://13.203.199.148:3000",   // React dev server
-  "https://nearbyhub.vercel.app/",  // production frontend
+  "https://13.203.199.148",   // React dev server
+  "https://nearbyhub.vercel.app",  // production frontend
 ];
 
 app.use(
